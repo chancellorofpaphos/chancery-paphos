8be1a03a954ba40aafa34e18c83ae2a419632ca0
chancellorofpaphos <chancellorofpaphos@protonmail.com>
2021-06-13 19:00:24 +0100

    Added script for merging PDFs.

--- inputs.py
+++ inputs.py
@@ -8,13 +8,13 @@ Configures the inputs for a given letters patent.
 
 # "peerage" indicates a patent granting a peerage or baronetage. "other"
 # indicates a miscelleneous type of patent.
-patent_type = "peerage"
+patent_type = "other"
 
-pino = 490
+pino = 500
 
 # Date stuff.
-day = 29
-month = "Primilis" # Write out the month's name in full.
+day = 3
+month = "Quartilis" # Write out the month's name in full.
 year = 8
 
 #######################
@@ -23,24 +23,16 @@ year = 8
 
 # If the letters patent are NOT for a peerage, leave well alone.
 
-grantee = "Michelle Anderson"
-# Either "male" of "female" - no non-binary nonsense!
-gender = "female"
-# Either "duke", "marquess", "earl", "viscount", "baron" or "baronet".
-degree = "baronet"
-title = "Baronet Anderson"
-# Use a list type. The order here is the order in which they'll appear on
-# the letters patent.
-subsidiary_titles = []
-# Should begin with "Whereas" and end with a comma.
-whereas = ("Whereas the gentlewoman known as Michelle Anderson is the "+
-           "first woman added to my Seraglio born on or after the first "+
-           "day of January in the year two thousand and one of the "+
-           "Gregorian calendar, which many take to be the first day of "+
-           "the twenty-first century, ")
+grantee = "Jacob Adams, Earl of the Valley"
+gender = "male" # Either "male" of "female" - no non-binary nonsense!
+degree = "baron" # Either "duke", "marquess", etc.
+title = "Baron of Adams"
+subsidiary_titles = [] # Use a list type.
+# The "whereas" should begin with "Whereas" and end with a comma.
+whereas = ("Whereas yesterday I issued letters patent creating the Barony of Adams, and promised to issue further letters patent granting said barony to another,")
 # Enter either a code for a standard remainder, or the custom remainder in
 # full, beginning with "and" and ending without punctuation.
-remainder = "heirs-male-and-bastards"
+remainder = "perpetual"
 
 #####################
 # OTHER-ONLY INPUTS #
@@ -49,13 +41,5 @@ remainder = "heirs-male-and-bastards"
 # If the letters patent are NOT of "other" type, leave well alone.
 
 filling = (
-    "Whereas my entirely beloved counsellor The Honourable The Chancellor "+
-    "of Paphos has humbly submitted the document known as The Statute of "+
-    "of San Fernando, which he published on the first day of March two "+
-    "thousand and twenty-one in the Gregorian calendar, to my Ducal "+
-    "consideration,\n\n"+
-    "Now be it enacted as follows:\n\n"+
-    "A new body politic, the { \hoskeroe County of San Fernando } by "+
-    "name, is hereby created within my Duchy of Paphos.\n\n"+
-    "The said document known as The Statute of San Fernando shall "+
-    "henceforth have the force of law.")
+    "Be it enacted as follows:\n\n"+
+    "The articles attached to these presents are hereby confirmed as the essential { \\hoskeroe Doctrine of the Religion of Paphos } which is the state ritual of this my Duchy of Paphos.")
